4cb4bb4f362536c65b5ce4919b768467a8bf74bf
arun <arunberry47@gmail.com>
2021-03-22 16:33:58 +0530

    styling button componenrt

--- src/components/ui/button.component.tsx
+++ src/components/ui/button.component.tsx
@@ -1,11 +1,15 @@
+import style from './button.module.scss'
 
 export interface ButtonProp {
     children: any;
+    className?: string;
     onClick?: (e: any) => void
 }
 
 export const Button = (props: ButtonProp) => {
-    return <button onClick={props.onClick}>
+    return <button 
+    className={`${style.button} ${props.className}`} 
+    onClick={props.onClick}>
         {props.children}
     </button>
 }
