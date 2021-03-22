a571e667255b83fbe14e67106f88fbab9d82b9ee
arun <arunberry47@gmail.com>
2021-03-22 15:59:20 +0530

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <button onClick={() => router.push("/products")}>
+          view products
+        </button>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ProductsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default ProductsPage;
